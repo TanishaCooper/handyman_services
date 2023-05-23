183c1efca6901c91af5c81d550e8bda68f720f85
Tanisha Cooper <strickland.tanisha@yahoo.com>
2023-05-22 20:02:29 -0400

    Add java

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Add your custom JavaScript here, such as form validation and payment processing
+// Create handyman image element
+const handymanImage = document.createElement('img');
+handymanImage.id = 'handyman-image';
+handymanImage.src = 'handyman.jpg';
+handymanImage.alt = 'Handyman';
+handymanImage.width = 200;
+handymanImage.height = 200;
+
+// Append handyman image to the header
+const header = document.querySelector('header');
+header.appendChild(handymanImage);
