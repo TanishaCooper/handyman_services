7ba5276ca5b7231ab241eac5ec109653f41f6d8b
Tanisha Cooper <strickland.tanisha@yahoo.com>
2023-05-22 20:50:40 -0400

    updates

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Create handyman image element
-const handymanImage = document.createElement('img');
-handymanImage.id = 'handyman-image';
-handymanImage.src = 'handyman.jpg';
-handymanImage.alt = 'Handyman';
-handymanImage.width = 200;
-handymanImage.height = 200;
 
-// Append handyman image to the header
-const header = document.querySelector('header');
-header.appendChild(handymanImage);
